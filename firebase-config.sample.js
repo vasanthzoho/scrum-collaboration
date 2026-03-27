6001a50b96474447738880b6440519fe17fdfa5f
Sujitha N <sujitha@hutechsolutions.com>
2026-03-27 22:17:27 +0530

    Restore secure Firebase config with local credentials

---
+++ firebase-config.sample.js
@@ -0,0 +1,11 @@
+// Sample config: copy and rename to firebase-config.local.js with your real credentials.
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
