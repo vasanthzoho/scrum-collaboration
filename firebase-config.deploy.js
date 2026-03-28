91ae7bad0e0d6b2e35160876fe71d16532b8a64e
Sujitha N <sujitha@hutechsolutions.com>
2026-03-28 13:20:59 +0530

    Calude fix changes done

---
+++ firebase-config.deploy.js
@@ -0,0 +1,14 @@
+// Deployment config: Add your real Firebase values for production deployment.
+// This file should NOT be committed to the repository.
+// Use this for GitHub Pages or other static hosting deployments.
+// Make sure to use your RESTRICTED API key here.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgbJ7-mBK1RgDZYIiJ8nTNdGP85rjiXGI",  // ← Replace with your restricted API key
+  authDomain: "scrum-collaboration.firebaseapp.com",
+  databaseURL: "https://scrum-collaboration-default-rtdb.firebaseio.com",
+  projectId: "scrum-collaboration",
+  storageBucket: "scrum-collaboration.firebasestorage.app",
+  messagingSenderId: "573194507689",
+  appId: "1:573194507689:web:1a535f52d4afc6a8fce0b8"
+};
